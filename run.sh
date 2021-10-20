18770f1d886ed1ffe0f8743b86030f5db7f9357d
Theo Portlock <zn.tportlock@gmail.com>
2021-10-20 12:14:34 +0200

    Implements dsl2 modules

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nextflow run main.nf\
 	-with-tower\
-	-c /home/theo/postdoc/rhadikaproj/PATCH-pipeline/configs/institutional/laptop.config\
-	-c /home/theo/postdoc/rhadikaproj/PATCH-pipeline/configs/conf/laprun.config
+	-c configs/institutional/uppmax.config\
+	-c configs/conf/upprun.config
